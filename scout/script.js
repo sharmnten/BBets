6026d9e661489c44c7b82909ab808f6c44ac8227
Neal Sharma <sharmnten@gmail.com>
2025-03-03 02:59:27 +0000

    Added vibration api support

---
+++ scout/script.js
@@ -0,0 +1,10 @@
+document.getElementById("button1").onclick = function() {
+    if (navigator.vibrate) {
+        navigator.vibrate(200);
+    }
+};
+document.getElementById("button2").onclick = function() {
+    if (navigator.vibrate) {
+        navigator.vibrate(200);
+    }
+};
