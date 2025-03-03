59bb9caace8f732921f7bfb536ab03517e43cbcd
Neal Sharma <sharmnten@gmail.com>
2025-03-03 21:02:12 +0000

    Fix error? Perchance perchance

--- scout/script.js
+++ scout/script.js
@@ -3,7 +3,13 @@ document.getElementById("reef").onclick = function() {
         navigator.vibrate(200);
     }
 };
-document.getElementById("algae").onclick = bubbleHapticEffect();
+document.getElementById("algae").onclick = function(){
+  if (navigator.vibrate) {
+    navigator.vibrate(200);
+  }
+}
+
+
 function bubbleHapticEffect() {
     // A series of vibration patterns to simulate a "bubbling" effect
     const vibrationPattern = [
